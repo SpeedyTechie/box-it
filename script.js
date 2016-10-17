de65e1608a7c0e8dca9783a09fd9e78ab09a15dd
Zane Harnish <public@zaneharnish.com>
2016-10-17 14:37:55 -0400

    Add basic init

--- script.js
+++ script.js
@@ -2,3 +2,32 @@
 box-it
 v0.1.0
 */
+
+;
+(function($) {
+    'use strict';
+ 
+    $.boxit = function(options) {
+        var boxitOptions = $.extend({
+            action: 'init'
+        }, options);
+        
+        if (boxitOptions.action === 'init') {
+            boxitInit(boxitOptions);
+        }
+    };
+    
+    function boxitInit(options) {
+        var boxitWrap = $('<div />');
+        boxitWrap.addClass('boxit-wrap');
+        
+        var boxitBox = $('<div />');
+        boxitBox.addClass('boxit-box');
+        
+        boxitWrap.appendTo('body');
+        boxitBox.appendTo(boxitWrap);
+        
+        boxitWrap.css('display', 'none');
+    }
+    
+}(jQuery));
